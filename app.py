860a355f88ad4067eeb3cc7cf931186f5c4e8520
Ian McGuier <mcguieri@gmail.com>
2013-08-22 19:33:55 -0400

    Wrote semi-functional html pages for add/remove person

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import flask
 from flask import Flask
-from flask import render_template,request,redirect,url_for,Response
+from flask import render_template,request,redirect,url_for,Response,jsonify
 from flask.ext.pymongo import PyMongo
 from random import choice
 
@@ -15,18 +16,27 @@ def primary():
 
 @app.route('/add')
 def addPersonPage():
-    return render_template('addPerson.html')
+    return render_template('addPerson.html')	
 
+
+@app.route('/remove')
+def removePerson():
+	return render_template('removePerson.html')
+
+@app.route('/allEmails')
+def getAllEmails():
+	js = flask.json.dumps([x['email'] for x in mongo.db.people.find()])
+	return Response(js,status=200,mimetype='application/json')
 
 
 #Add a person to the python database
 #Return true if added, false if they've already exist
 def addPerson(first_name,last_name,email_address,department):
-	if None == mongo.people.find_one({"email": email_address}):
+	if None == mongo.db.people.find_one({"email": email_address}):
             entry = {"first": first_name,"last": last_name,
                      "email": email_address,"department": department, 
                      "priority": 1}
-            mongo.people.insert(entry)
+            mongo.db.people.insert(entry)
             return true
         else:
             return false
@@ -40,15 +50,15 @@ def addPerson(first_name,last_name,email_address,department):
 #database with 0 priority
 #Randomly select a new group of people to go to lunch respecting priorities
 def createNewLunchSet(number_participants):
-	if mongo.ls.find().count() != 0:
-		for person in mongo.ls.find():
-			mongo.people.update({"email": person["email"]}, 
+	if mongo.db.ls.find().count() != 0:
+		for person in mongo.db.ls.find():
+			mongo.db.people.update({"email": person["email"]}, 
 								{"$set": {"priority": 0}})
-			mongo.ls.remove(person)
+			mongo.db.ls.remove(person)
 
-	addToLunchSet(number_participants)
+	lunchList = addToLunchSet(number_participants)
 	updatePriority()
-	return []
+	return lunchList
 
 
 
@@ -56,12 +66,13 @@ def createNewLunchSet(number_participants):
 #If this person is in the current lunch set
 #put them back into the main database at their previous priority
 #select someone else randomly to take their place in this lunch set
+#returns true upon success, false upon failure
 def skipThisPerson(email):
-    person = mongo.ls.find({"email": email})
+    person = mongo.db.ls.find({"email": email})
     if person == None:
     	return false
-    mongo.people.update({"email": email}, {"$set": {"priority": person["priority"]+1}})
-    mongo.ls.remove({"email": email})
+    mongo.db.people.update({"email": email}, {"$set": {"priority": person["priority"]+1}})
+    mongo.db.ls.remove({"email": email})
     addToLunchSet(1)
     return true
 
@@ -69,9 +80,10 @@ def skipThisPerson(email):
 
 
 #Add specified number of people to the lunch set
-#Returns true upon success
+#Returns list of lunch set
 def addToLunchSet(number_of_additions):
-	eligable = mongo.people.find({"priority": {"$gt": 0}})
+	eligable = mongo.db.people.find({"priority": {"$gt": 0}})
+	lunchList = []
 	weightedList = []
 	for entry in eligable:
 		i = entry["priority"]
@@ -80,13 +92,15 @@ def addToLunchSet(number_of_additions):
 
 	for k in range(number_of_additions):
 		selected = choice(weightedList)
-		mongo.ls.insert(mongo.people.find({"email" :selected["email"]}))
+		person = [x for x in eligable if x["email"] == selected].pop()
+		mongo.db.ls.insert(person)
+		lunchList.append(person)
 		try:
 			while(1):
 				weightedList.remove(selected)
 		except:
 			pass	
-	return true
+	return lunchList
 
 
 
@@ -94,11 +108,11 @@ def addToLunchSet(number_of_additions):
 #remove this person entirely from the database.
 #Return true upon success, false when email doesn't exist
 def removePerson(email):
-	if mongo.ls.find({"email": email}) != None:
+	if mongo.db.ls.find({"email": email}) != None:
 		skipThisPerson(email)		
 
 	try:
-		mongo.people.remove({"email": email})
+		mongo.db.people.remove({"email": email})
 	except:
 		return false
 	return true
@@ -109,9 +123,9 @@ def removePerson(email):
 #Update priorities of people not chosen in the lunch set
 #Returns true upon success
 def updatePriority():
-	for person in mongo.people.find():
-		if mongo.ls.find(person) == None:
-			mongo.people.update({"email": person["email"]},
+	for person in mongo.db.people.find():
+		if mongo.db.ls.find(person) == None:
+			mongo.db.people.update({"email": person["email"]},
 								{"$set": {"priority": person["priority"]+1}})
 	return true
     
