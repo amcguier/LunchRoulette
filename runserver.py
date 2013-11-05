cb8e8237dabef524c0d2d6f222aa817711010a68
Ian McGuier <mcguieri@gmail.com>
2013-11-05 09:54:48 -0500

    Fixed template e-mail up and changed server to live

--- runserver.py
+++ runserver.py
@@ -1,3 +1,4 @@
 from LunchRoulette import app
-app.run(host='192.168.0.27') #Use this to run on greatwhite (or whatever server)
+app.run(host='192.168.0.27')
+#app.run(host="192.168.110.37") #to run from Nautilus
 #app.run(debug=True) #Use to run on localhost for debugging purposes
